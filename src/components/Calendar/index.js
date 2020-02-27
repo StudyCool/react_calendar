3a7202b3448452d461d1decf0d1714f4e77647d3
StudyCool <as.shevch@gmail.com>
2020-02-27 14:48:30 +0200

    create structure of project

---
+++ src/components/Calendar/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import styles from './Calendar.module.css';
+
+
+
+class Calendar extends Component {
+  constructor (props) {
+    super(props);
+  }
+
+}
+
+export default Calendar;
---
+++ src/components/Date/index.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import styles from './Date.module.css';
+
+class Date extends Component {
+  constructor (props) {
+    super(props);
+  }
+}
+
+export default Date;
