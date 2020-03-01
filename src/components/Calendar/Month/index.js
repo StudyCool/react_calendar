61eddabf3154b6660e7a9f54c518202775b6572f
StudyCool <as.shevch@gmail.com>
2020-03-01 09:14:24 +0200

    add Month component

--- src/components/Calendar/Month/index.js
+++ src/components/Calendar/Month/index.js
@@ -3,7 +3,7 @@ import styles from './Month.module.scss';
 import moment from "moment";
 import Week from "../Week";
 
-class Month extends Component {
+/*class Month extends Component {
     constructor(props) {
         super(props);
 
@@ -12,12 +12,13 @@ class Month extends Component {
     renderMonth = () => {
         let month = 5;
         let daysOfMonth = [];
-        let i= moment().startOf('month');
-        console.log(i);
-        for (i;  moment().daysInMonth('month'); i++){
-         //   daysOfMonth = daysOfMonth.push(i);
+//let i = moment().startOf(5); i <= moment().daysInMonth(5); i++
+
+        for (let i = 1; i <= 31; i++) {
+            daysOfMonth = daysOfMonth.push(i);
+            console.log(daysOfMonth)
         }
-      // console.log(daysOfMonth)
+        // console.log(daysOfMonth)
         return daysOfMonth;
 
     }
@@ -32,7 +33,19 @@ class Month extends Component {
 
         );
     }
-}
+}*/
+let month = "June";
+const firstMonthDay = moment().startOf("month");
+console.log(firstMonthDay);
 
+const Month = (month) => {
+    return (
+        <div>
+            {
+
+            }
+        </div>
+    );
+};
 
 export default Month;
